45fd3f4bd3ed226205348d0e002aa0f1fdf116f0
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2013-03-14 14:55:10 +0000

    Moved to function

--- plots/movingavg.R
+++ plots/movingavg.R
@@ -1,19 +1,22 @@
 # Plot some data and a moving average on top of it
 # Assumes:
 #	1. Data Format: csv data with X, Y headers
-#	2. Data is over 24 hours
+#	2. Data is over 24 hours (hence labels, xlim)
 #	3. Moving Average is 10 sample, 2-sided
 
+plotWithMovingAvg <- function(data)
+{
+	plot(data$X, data$Y, col=grey(.5), type="l", axes=F, xlim=c(0,24))
+	axis(1, labels=0:24, at=0:24)
+	axis(2)
+	box()
+	grid()
+
+	f <- rep(1/10, 10)
+	y_lag <- filter(data$X, f, sides=2)
+
+	lines(data$X, y_lag, col="red", lwd=2)
+}
+
 data <- read.table(file.choose(), sep=",", header=T)
-
-plot(data$X, data$Y, col=grey(.5), type="l", axes=F, xlim=c(0,24))
-axis(1, labels=0:24, at=0:24)
-axis(2)
-box()
-grid()
-
-f <- rep(1/10, 10)
-y_lag <- filter(a$prob, f, sides=2)
-
-lines(a$hour, y_lag, col="red", lwd=2)
-
+plotWithMovingAvg(data)
