9c7995a943e1f5e4a77aee970b9891856198f758
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2013-03-14 15:03:23 +0000

    Added (untested) London dots

---
+++ maps/londonDots.R
@@ -0,0 +1,25 @@
+# Warning: untested
+
+londonMap <- function(data)
+{
+	London <- GetMap(center=c(51.514910,-0.126858), zoom =12, destfile = "London.jpg", maptype = "terrain")
+	colors <- c("darkred", "darkblue", "darkgreen", "black", "darkorange", "purple")
+	
+	par(omi=c(0,0,0,0), mai=c(0,0,0,0))
+	PlotOnStaticMap(London, data$lat[1], data$lon[1], col=colors[data$group[1]], cex=1, pch=19)
+	for (i in 2:length(data$lat))
+	{
+		PlotOnStaticMap(London, data$lat[1], data$lon[1], col=colors[data$group[1]], cex=1, add=T)
+	}
+}
+
+
+# Data format: csv
+# Columns:
+#	lat, lon, group_id
+
+library(RgoogleMaps)
+library(ReadImages)
+
+data <- read.table(file.choose(), sep=",")
+londonMap(data)
