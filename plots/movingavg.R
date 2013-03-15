c5c36a7f4f1da547a7373d74fa5c2bf4179a8128
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2013-03-15 11:52:05 +0000

    bug fix

--- plots/movingavg.R
+++ plots/movingavg.R
@@ -4,19 +4,21 @@
 #	2. Data is over 24 hours (hence labels, xlim)
 #	3. Moving Average is 10 sample, 2-sided
 
-plotWithMovingAvg <- function(data)
+plotWithMovingAvg <- function(data, xlab, ylab)
 {
-	plot(data$X, data$Y, col=grey(.5), type="l", axes=F, xlim=c(0,24))
+	plot(data$X, data$Y, col=grey(.5), type="l", axes=F, xlim=c(0,24), xlab=xlab, ylab=ylab)
 	axis(1, labels=0:24, at=0:24)
 	axis(2)
 	box()
 	grid()
 
 	f <- rep(1/10, 10)
-	y_lag <- filter(data$X, f, sides=2)
+	y_lag <- filter(data$Y, f, sides=2)
 
 	lines(data$X, y_lag, col="red", lwd=2)
 }
 
 data <- read.table(file.choose(), sep=",", header=T)
-plotWithMovingAvg(data)
+xlab = ""
+ylab = ""
+plotWithMovingAvg(data, xlab, ylab)
